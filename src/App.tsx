68b039215bc90c403f38c7a615d25df9a7914578
Kenneth Pinzon <kennethpinzonrojas@gmail.com>
2022-12-23 01:12:45 -0600

    rudimentary dropdown component static

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import { useState } from 'react'
-
+import {DropDown} from './DropDown'
 import './App.css'
 
 function App() {
   const logo_image_path = "https://www.freeiconspng.com/uploads/pokeball-pokemon-ball-picture-11.png";
-
+  const pokemon_card_image_path = "https://assets1.ignimgs.com/2019/01/30/6-pikarom-d-1548807073825.png";
   return (
-    <div className='flex flex-row items-center font-sans'>
+    <div className='flex flex-row items-start font-sans'>
 
       <div className='p-4 space-y-8 content-center items-center'>
         <img src={logo_image_path} alt="logo" className = "transform -scale-x-100 object-scale-down h-40 w-40"/>
         <h1 className='text-center text-2xl font-extrabold'> Pokemon Pulls</h1>
+      </div>
+
+      <div>
+        <DropDown/>
+        <img src = {pokemon_card_image_path}
+         alt = "Pikachu and Zekrom GX Rainbow SR Pokemon Card" 
+         className='object-scale-down h-80'>
+         </img>
+      </div>
+        
+      <div>
+
       </div>
 
     </div>
---
+++ src/DropDown.tsx
@@ -0,0 +1,60 @@
+import {Menu} from '@headlessui/react'
+
+
+export const DropDown = () => {
+
+
+    return(
+        <div className="min-h-screen bg-indigo-100 flex justify-end p-4">
+            <Menu as='div' className="relative">
+
+                {/*Menu Button*/}
+                <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300
+                    shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50
+                    focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 
+                    focus: ring-indigo-500"
+                    >
+                        Options Menu        
+                        <i className="fa-solid fa-chevron-down -mr-1 m1-2 h-5 w-5" aria-hidden = "true" />
+                </Menu.Button>
+
+                {/*Menu Items*/}
+                    <Menu.Items className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg
+                    bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100
+                    focus:outline-none'>
+
+                        <div className='py-1'>
+                            <Menu.Item>
+                                {({active}) =>(
+
+                                    <h3 className={`group flex items-center px-4 text-sm 
+                                    ${active ? "bg-indigo-500 text-white": " text-gray-700"}`}>
+                                        Item One
+                                    </h3>
+
+                                )} 
+                            
+                            </Menu.Item>
+                        </div>
+                        
+                        <div className='py-1'>
+                            <Menu.Item>
+                                {({active}) =>(
+
+                                    <h3 className={`group flex items-center px-4 text-sm 
+                                    ${active ? "bg-indigo-500 text-white": "text-gray-700"}`}>
+                                         Item Two
+                                     </h3>
+                                )}
+                                
+                            </Menu.Item>
+                        </div>
+
+                    </Menu.Items>
+               
+
+            </Menu>
+        </div>
+    );
+}
+
