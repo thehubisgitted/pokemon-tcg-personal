c5bee8c00d6efc3cd1d13c7d3805f59b12ae80b6
Kenneth Pinzon <kennethpinzonrojas@gmail.com>
2023-01-10 15:59:00 -0600

    base information display and card information v0

--- src/APIUtility.tsx
+++ src/APIUtility.tsx
@@ -2,16 +2,30 @@ import axiosConfig from "./axiosConfig";
 import { PokemonTCG } from "pokemon-tcg-sdk-typescript";
 
 type getListI = {
-  data: String[];
+  data: PokemonTCG.Rarity[];
 };
 
+export interface CardInfoI {
+  id:String;
+  name:String;
+  evolvesFrom?: String;
+  evolvesTo?: String[];
+  number: String;
+  artist?: String;
+  rarity: String;
+  flavorText?: String;
+  nationalPokedexNumbers?: number[];
+  images: PokemonTCG.CardImage;
+  tcgplayer?: PokemonTCG.TCGPlayer;
+}
+
+
 type getCardsI = {
-  data: PokemonTCG.Card[];
+  data: CardInfoI[];
   page: number;
   pageSize: number;
   count: number;
   totalCount: number;
-
 };
 
 type getSetsI = {
@@ -31,12 +45,13 @@ export interface setInformatonI {
   images: PokemonTCG.SetImage;
 }
 
-export const getRarities = async (): Promise<String[]> => {
-  const rarities: String[] = await axiosConfig
+export const getRarities = async (): Promise<PokemonTCG.Rarity[]> => {
+  const rarities: PokemonTCG.Rarity[] = await axiosConfig
     .get<getListI>("/rarities")
     .then((response) => {
       const data = response.data;
       const rarities = response.data.data;
+      console.log(rarities);
       return rarities;
     });
 
@@ -53,10 +68,17 @@ export const getSets = async (): Promise<setInformatonI[]> => {
   let totalCount: number = 1;
   let set_info_array: setInformatonI[] = [];
 
-  const banned_keywords: String[] = ["*Promo*", "*Mcdonald*", "*POP*", "*FUTSAL*", "*Trainer Gallery", "Classic Collection"];
-
-  let q_parameter:String = `-name%3A%22${banned_keywords.join('%22%20-name%3A%22')}%22`
-  console.log(q_parameter);
+  const banned_keywords: String[] = [
+    "*Promo*",
+    "*Mcdonald*",
+    "*POP*",
+    "*FUTSAL*",
+    "*Trainer Gallery",
+    "Classic Collection",
+  ];
+  const q_parameter: String = `-name%3A%22${banned_keywords.join(
+    "%22%20-name%3A%22"
+  )}%22`;
 
   do {
     try {
@@ -94,29 +116,47 @@ export const getSets = async (): Promise<setInformatonI[]> => {
   return set_info_array;
 };
 
-export const getCards = async (rarity: String, setID: String) => {
-
+export const getCardsbySetID = async (setID: String) => {
   let current_page: number = 1;
   let count: number = 1;
   let totalCount: number = 1;
 
-  do{
-    try{
-      const query_data = await axiosConfig.get<getCardsI>(`/cards?page=${current_page.toString()}&q=set.id:${setID} rarity:${rarity}`)
-      .then((response)=>{
-        return response.data;
+  const select_parameters: String[] = [
+    "id",
+    "name",
+    "evolvesTo",
+    "evolvesFrom",
+    "number",
+    "artist",
+    "rarity",
+    "flavorText",
+    "nationalPokedexNumbers",
+    "images",
+    "tcgplayer",
+  ];
+  const select_query: String = select_parameters.join(",");
+
+  do {
+    try {
+      const query_data = await axiosConfig
+        .get<getCardsI>(
+          `/cards?page=${current_page.toString()}&q=set.id:${setID}&select=${select_query}`
+        )
+        .then((response) => {
+          return response.data;
+        });
+
+      const cards_data: CardInfoI[] = query_data.data;
+
+      cards_data.map((card) => {
+        //console.log(JSON.stringify(card));
       });
 
-      const cards_data:PokemonTCG.Card[] = query_data.data;
-
-      cards_data.map((card)=>{
-        console.log(JSON.stringify(card));
-      })
-      
-    }
-    catch(e){
+      count = query_data.count;
+      totalCount = query_data.totalCount;
+      current_page += 1;
+    } catch (e) {
       console.error(`Oops, something went wrong ${e}`);
     }
-
-  }while(false);
+  } while (count < totalCount);
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,57 @@
 import { useEffect, useMemo, useState } from "react";
 import { DropDown } from "./components/DropDown";
-import { getCards, getRarities, getSets, setInformatonI } from "./APIUtility";
+import {
+  CardInfoI,
+  getCardsbySetID,
+  getRarities,
+  getSets,
+  setInformatonI,
+} from "./APIUtility";
 import "./App.css";
 import { PokemonTCG } from "pokemon-tcg-sdk-typescript";
+import { Rarity } from "pokemon-tcg-sdk-typescript/dist/sdk";
 
 function App() {
   const logo_image_path =
     "https://www.freeiconspng.com/uploads/pokeball-pokemon-ball-picture-11.png";
-  const pokemon_card_image_path =
+  const pikachu_zekrom_card_image_path =
     "https://assets1.ignimgs.com/2019/01/30/6-pikarom-d-1548807073825.png";
+
+  const placeholder_card:CardInfoI = {
+    id: "swsh4-25",
+    name: "Charizard",
+    evolvesFrom: "Charmeleon",
+    number: "25",
+    artist: "Ryuta Fuse",
+    rarity: "Rare",
+    flavorText:
+      "It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.",
+    nationalPokedexNumbers: [6],
+    images: {
+      small: "https://images.pokemontcg.io/swsh4/25.png",
+      large: "https://images.pokemontcg.io/swsh4/25_hires.png",
+    },
+    tcgplayer: {
+      url: "https://prices.pokemontcg.io/tcgplayer/swsh4-25",
+      updatedAt: "2021/08/04",
+      prices: {
+        normal: {
+          low: 1.73,
+          mid: 3.54,
+          high: 12.99,
+          market: 2.82,
+          directLow: 3.93,
+        },
+        reverseHolofoil: {
+          low: 3,
+          mid: 8.99,
+          high: 100,
+          market: 3.89,
+          directLow: 4.46,
+        },
+      },
+    },
+  }
 
   const [current_set, Set_current_set] = useState<setInformatonI>({
     id: "base1",
@@ -21,7 +64,42 @@ function App() {
       logo: "https://images.pokemontcg.io/base1/logo.png",
     },
   });
-  const [card, Set_Card] = useState<unknown>();
+
+  const [current_card, Set_current_card] = useState<CardInfoI>({
+    id: "swsh4-25",
+    name: "Charizard",
+    evolvesFrom: "Charmeleon",
+    number: "25",
+    artist: "Ryuta Fuse",
+    rarity: "Rare",
+    flavorText:
+      "It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.",
+    nationalPokedexNumbers: [6],
+    images: {
+      small: "https://images.pokemontcg.io/swsh4/25.png",
+      large: "https://images.pokemontcg.io/swsh4/25_hires.png",
+    },
+    tcgplayer: {
+      url: "https://prices.pokemontcg.io/tcgplayer/swsh4-25",
+      updatedAt: "2021/08/04",
+      prices: {
+        normal: {
+          low: 1.73,
+          mid: 3.54,
+          high: 12.99,
+          market: 2.82,
+          directLow: 3.93,
+        },
+        reverseHolofoil: {
+          low: 3,
+          mid: 8.99,
+          high: 100,
+          market: 3.89,
+          directLow: 4.46,
+        },
+      },
+    },
+  });
 
   const [tcg_sets, Set_tcg_sets] = useState<setInformatonI[]>([
     {
@@ -37,11 +115,16 @@ function App() {
     },
   ]);
 
-  const [card_rarities, Set_card_rarities] = useState<String[]>([
-    "rare",
-    "uncommon",
-    "common",
+  const [card_rarities, Set_card_rarities] = useState<PokemonTCG.Rarity[]>([
+    Rarity.Common,
+    Rarity.Uncommon,
+    Rarity.Rare,
+    Rarity.Promo,
   ]);
+
+  
+
+  const rarity_to_card_dictionary = useState<Map<PokemonTCG.Rarity,CardInfoI>>();
 
   const fetchSetInformationAndSetState = async (
     API_Promise: Promise<setInformatonI[]>,
@@ -56,8 +139,8 @@ function App() {
   };
 
   const fetchRaritiesAndSetState = async (
-    API_Promise: Promise<String[]>,
-    setState: React.Dispatch<React.SetStateAction<String[]>>
+    API_Promise: Promise<Rarity[]>,
+    setState: React.Dispatch<React.SetStateAction<Rarity[]>>
   ) => {
     try {
       const data = await API_Promise;
@@ -81,19 +164,28 @@ function App() {
           className="transform -scale-x-100 object-scale-down h-40 w-40 my-2"
         />
         <h1 className=" text-2xl font-extrabold "> Pokemon Pulls</h1>
+        <div className="mt-20 text-center">
+          <h2> Card Information </h2>
+          <h3>{current_card.name} Num. {current_card.number}{" "}</h3>
+          {current_card.flavorText? <h3>{current_card.flavorText}</h3> :""}
+          <div className="flex-row px-4">
+            {current_card.evolvesFrom? <h3>Evolves From {current_card.evolvesFrom}</h3>:""}
+            {current_card.evolvesTo? <h3>Evolves To {current_card.evolvesTo}</h3>:""}
+          </div>
+            
+        </div>
       </div>
 
       <div className="my-12 w-1/3 space-y-4 items-center">
-
         <img
-          src={pokemon_card_image_path}
-          alt="Pikachu and Zekrom GX Rainbow SR Pokemon Card"
+          src={current_card.images.large}
+          alt="Current Pokemon Card"
           className="object-scale-down w-7/12 block mx-auto"
         ></img>
- 
+
         <div className="justify-center text-center">
           <button
-            onClick={() => getCards("Rare Holo EX", "g1")}
+            onClick={() => getCardsbySetID("g1")}
             className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 mx-1 px-4 rounded"
           >
             {" "}
@@ -112,7 +204,17 @@ function App() {
       </div>
 
       <div className="w-1/3 py-10 justify-center">
-        <img src= {current_set.images.logo} alt = "logo" className = "object-scale-down w-2/3 block mx-auto"></img>
+        <img
+          src={current_set.images.logo}
+          alt="logo"
+          className="object-scale-down w-2/3 block mx-auto"
+        ></img>
+
+        <div className="mt-10 text-center">
+          <h2>Series: {current_set.series}</h2>
+          <h2>Release Date: {current_set.releaseDate}</h2>
+          <h2>Total Sets Printed: {current_set.printedTotal}</h2>
+        </div>
       </div>
     </div>
   );
